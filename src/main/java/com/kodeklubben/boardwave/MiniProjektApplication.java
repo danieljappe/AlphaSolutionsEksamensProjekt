31c775648d8fa964621004521ef354a16fcbbdd9
fred077a <fred18q3@stud.kea.dk>
2023-04-25 11:28:58 +0200

    fixed typo

--- src/main/java/com/kodeklubben/boardwave/MiniProjektApplication.java
+++ src/main/java/com/kodeklubben/boardwave/MiniProjektApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiniProjektApplication {
+public class BoardWaveApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MiniProjektApplication.class, args);
+        SpringApplication.run(BoardWaveApplication.class, args);
     }
 
 }
