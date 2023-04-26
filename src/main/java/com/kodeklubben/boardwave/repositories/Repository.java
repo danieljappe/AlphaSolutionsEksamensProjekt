6bc9b9c4e2a59ba6d90f0e8de3908c79fb9036f4
fred077a <fred18q3@stud.kea.dk>
2023-04-26 10:17:06 +0200

    added method to Repository.java that returns a user with email and password. Id of user is -1 if it was wrong.

--- src/main/java/com/kodeklubben/boardwave/repositories/Repository.java
+++ src/main/java/com/kodeklubben/boardwave/repositories/Repository.java
@@ -1,8 +1,34 @@
 package com.kodeklubben.boardwave.repositories;
+import com.kodeklubben.boardwave.models.User;
 import com.kodeklubben.boardwave.services.DatabaseConnectionManager;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @org.springframework.stereotype.Repository
 public class Repository {
     private final DatabaseConnectionManager dcm = new DatabaseConnectionManager("eu-west.connect.psdb.cloud", "n9yvymfj507ekkjlhtkd", "pscale_pw_iJs7WNJ0mxHfCdADkqSRM1GGcSUK5GgNiKGcgzgSz5m");
+
+    private static final String GET_USER = "SELECT id, name, email, password FROM users WHERE id=?";
+
+    User loginWithEmailAndPassword(String email, String password) {
+        try (PreparedStatement preparedStatement = dcm.getConnection().prepareStatement(GET_USER)) {
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            User user;
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                user = new User(resultSet.getString("name"), resultSet.getString("password"), resultSet.getString("email"), resultSet.getInt("id"));
+            } else {
+                user = new User("", "", "", -1);
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 
 }
